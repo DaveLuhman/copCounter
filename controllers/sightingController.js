f1c891198b0f0397ea8f2c8ba6dd9c7a76f8354b
Dave Luhman <Dave@ADO.software>
2022-07-10 10:47:35 -0500

    moved function from routes to controllers

--- controllers/sightingController.js
+++ controllers/sightingController.js
@@ -4,20 +4,20 @@ const Sighting = require('../models/sightingModel')
 const controller = {}
 controller.getSightings = async (req, res) => {
     const sightings = await Sighting.find()
-    res.status(201).json(sightings)
+    res.status(201).json(sightings).render('index', {layout: 'main'})
     console.log(sightings)
 }
 controller.insertSighting = async (req, res) => {
     const sighting = await Sighting.create(req.body)
-    res.status(201).json(sighting)
+    res.status(201).json(sighting).render('index', {layout: 'main'})
 }
 controller.lightsOn = async (req, res) => {
     const sighting = await Sighting.create({ "lightsOn": true })
-    res.status(201).render('index')
+    res.status(201).render('index').render('index', {layout: 'main'})
 }
 controller.lightsOff = async (req, res) => {
     const sighting = await Sighting.create({ "lightsOn": false })
-    res.status(201).render('index')
+    res.status(201).render('index').render('index', {layout: 'main'})
 }
 
 module.exports = controller;
--- routes/sightingRoutes.js
+++ routes/sightingRoutes.js
@@ -3,9 +3,7 @@ const controller = require('../controllers/sightingController')
 const { model } = require('mongoose')
 const Sighting = require('../models/sightingModel')
 
-router.get('/', controller.getSightings, (req, res) => {
-    res.render('index', {layout: 'main'})
- })
+router.get('/', controller.getSightings)
 
 
 router.get('/SawOneLightsOn' , controller.lightsOn)
