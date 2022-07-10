572e462366e97211b894d4bc1ad67d4bfd8c9002
Dave Luhman <Dave@ADO.software>
2022-07-10 11:11:56 -0500

    they're not promises i guess

--- controllers/sightingController.js
+++ controllers/sightingController.js
@@ -4,20 +4,19 @@ const Sighting = require('../models/sightingModel')
 const controller = {}
 controller.getSightings = async (req, res) => {
     const sightings = await Sighting.find()
-    res.render('index', {layout: 'main'}).status(200)
-    console.log(sightings)
+    res.render('index', {layout: 'main'})
 }
 controller.insertSighting = async (req, res) => {
     const sighting = await Sighting.create(req.body)
-    res.status(201).json(sighting).render('index', {layout: 'main'})
+    res.status(201).render('index', {layout: 'main'})
 }
 controller.lightsOn = async (req, res) => {
     const sighting = await Sighting.create({ "lightsOn": true })
-    res.status(201).render('index').render('index', {layout: 'main'})
+    res.status(201).render('index')
 }
 controller.lightsOff = async (req, res) => {
     const sighting = await Sighting.create({ "lightsOn": false })
-    res.status(201).render('index').render('index', {layout: 'main'})
+    res.status(201).render('index')
 }
 
 module.exports = controller;
