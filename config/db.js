74e478d7447f1a680e5151479ba6ceb7346cefd0
Dave Luhman <Dave@ADO.software>
2022-07-14 17:23:39 -0500

    db config

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+      authSource: 'admin',
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
