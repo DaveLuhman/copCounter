fea2fa81c2bd73d6d3304b3461c0b534768223c5
Dave Luhman <Dave@ADO.software>
2022-07-14 02:49:29 -0500

    using mongoURI for auth source

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {auth:{authdb: 'admin'}})
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         mongoose.set('debug', true);
         console.log(`MongoDB Connected Successfully; MongoDB available`.cyan.underline)
     } catch (error) {
