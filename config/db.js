254bf6ae52d1eddd3f8954257a71c44208fd2fa7
Dave Luhman <Dave@ADO.software>
2022-07-14 17:33:14 -0500

    db config user client flag

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
+      userMongoClient: true,
       authSource: 'admin',
       useNewUrlParser: true,
       useUnifiedTopology: true,
